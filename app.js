ab7541d22f17e141aa45d3a8a0da9ea3ecafe8dc
Sharva Poshe <sharvaposhe@gmail.com>
2026-03-19 10:32:26 +0000

    Add webpages and routing

--- app.js
+++ app.js
@@ -3,10 +3,33 @@ const express = require('express');
 
 //defines where server is hosted
 const app = express();
+//listen for requests on port 3000
 app.listen(3000);
+//register view engine
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-    res.send('Hello')
+    res.render('index', { title: 'Home' })
 
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about', { title: 'About' })
+});
+
+app.get('/blogs', (req, res) => {
+
+    res.render('blogs', { title: 'All Blogs' })
+});
+
+app.get('/blogs/create', (req, res) => {
+
+    res.render('create', { title: 'Create a new Blog' })
+});
+
+//404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' })
 });
